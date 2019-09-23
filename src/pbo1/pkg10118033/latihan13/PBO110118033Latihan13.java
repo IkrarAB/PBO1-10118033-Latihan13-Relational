4748dff73c7446f385a5db48391cf0172b619ec3
IkrarAB <ikraranugrah26@gmail.com>
2019-09-23 23:12:26 +0700

    Initial commit

---
+++ src/pbo1/pkg10118033/latihan13/PBO110118033Latihan13.java
@@ -0,0 +1,23 @@
+package pbo1.pkg10118033.latihan13;
+
+/*
+ *
+ * @author
+ * NAMA  : Ikrar Anugrah Bastary
+ * KELAS : PBO1
+ * NIM   : 10118033
+ *
+ */
+
+public class PBO110118033Latihan13 {
+    public static void main(String[] args) {
+        int bil1 = 10;
+        int bil2 = 20;
+        System.out.println("a == b = " + (bil1==bil2));
+        System.out.println("a != b = " + (bil1!=bil2));
+        System.out.println("a > b = " + (bil1>bil2));
+        System.out.println("a < b = " + (bil1<bil2));
+        System.out.println("a >= b = " + (bil1>=bil2));
+        System.out.println("a <= b = " + (bil1<=bil2));
+    }
+}
